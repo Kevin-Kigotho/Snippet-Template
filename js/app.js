e2bd8b5d96a5a7daf6ffdbfaa4b8f4dd6c079e53
Kevin Kigotho <87012866+Kevin-Kigotho@users.noreply.github.com>
2022-07-15 22:05:31 +0300

    Version 1

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links a');
+
+    
+    burger.addEventListener('click', ()=>{
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index)=> {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease-in-out forwards ${index / 4 + 0.5 }s`;
+            }
+            // console.log(index / 7);
+        });
+
+        // Burger Animation
+        burger.classList.toggle('toggle')
+    });
+
+    
+
+
+}
+
+navSlide();
